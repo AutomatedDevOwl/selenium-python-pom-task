9ca4f803eb08dd463fce927190e42b8b2caa2315
automatedowl <automatedowl@yandex.com>
2019-08-28 16:37:41 +0300

    fixed lint.

--- dev/test/cfg/test_configuration.py
+++ dev/test/cfg/test_configuration.py
@@ -2,14 +2,15 @@ from selenium import webdriver
 from sys import platform
 import os
 
+
 class TestConfiguration:
-    
+
     def get_driver(self):
         if platform == "linux" or platform == "linux2":
             return webdriver.Chrome(
                 os.path.join(os.getcwd(),
-                    "driver","executable","chromedriver"))
+                             "driver", "executable", "chromedriver"))
         elif platform == "win32":
             return webdriver.Chrome(
                 os.path.join(os.getcwd(),
-                    "driver","executable","chromedriver.exe"))
+                             "driver", "executable", "chromedriver.exe"))
